520a3c0192d6a698ae79b5af1bb9b83ec10558a3
Hexin Zheng <hzhe5094@uni.sydney.edu.au>
2024-05-10 23:42:49 +1000

    Update

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -2,8 +2,8 @@ import {
   Avatar,
   AvatarFallback,
   AvatarImage,
-} from "@/registry/new-york/ui/avatar"
-import { Button } from "@/registry/new-york/ui/button"
+} from "./ui/avatar"
+import { Button } from "./ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,7 +13,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from "@/registry/new-york/ui/dropdown-menu"
+} from "./ui/dropdown-menu"
 
 export function UserNav() {
   return (
